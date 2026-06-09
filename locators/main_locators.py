069009b424402b31b4216fda12fb535c3335df31
Dimitri Samosiuk <imsweet75@gmail.com>
2026-06-09 21:46:34 +0300

    errors have been fixed, locators have been added

---
+++ locators/main_locators.py
@@ -0,0 +1,13 @@
+import os
+from pages.elements import WebElement, ManyWebElements
+from pages.base_page import WebPage
+
+
+class MainPage(WebPage):
+    def __init__(self, web_driver, url=''):
+        if not url:
+            url = os.getenv("MAIN_PAGE") or 'https://www.whitehouse.gov/'
+
+        super().__init__(web_driver, url)
+
+    btn_news = WebElement(xpath='(//*[@class="class="menu-item menu-item-type-post_type menu-item-object-page menu-item-37""])')
--- pages/header_page.py
+++ pages/header_page.py
@@ -1,34 +1,34 @@
-from selenium.webdriver.common.by import By
-
-class HeaderPage:
-
-    MENU_BUTTON = (By.XPATH, "//div[@class='wp-block-whitehouse-header__menu-toggle']//button")
-
-    LOGO = (By.XPATH, "//a[@class='wp-block-whitehouse-header__logo']")
-
-    SEARCH_BUTTON = (By.XPATH, "//div[@class='wp-block-whitehouse-header__search-toggle']//button")
-
-    NEWS = (By.XPATH, "//ul[@id='menu-primary-navigation']//a[contains(text(), 'News')]")
-    GALLERY = (By.XPATH, "//ul[@id='menu-primary-navigation']//a[contains(text(), 'Gallery')]")
-    LIVESTREAM = (By.XPATH, "//ul[@id='menu-primary-navigation']//a[contains(text(), 'Livestream')]")
-    TRUMP_ACCOUNTS = (By.XPATH, "//ul[@id='menu-primary-navigation']//a[contains(text(), 'Trump Accounts')]")
-    SAVE_AMERICA = (By.XPATH, "//ul[@id='menu-primary-navigation']//a[contains(text(), 'SAVE America')]")
-    INVESTMENTS = (By.XPATH, "//ul[@id='menu-primary-navigation']//a[contains(text(), 'Investments')]")
-    CONTACT = (By.XPATH, "//ul[@id='menu-primary-navigation']//a[contains(text(), 'Contact')]")
-
-    def __init__(self, driver):
-        self.driver = driver
-
-    def get_header_elements(self):
-        return [
-            self.driver.find_element(*self.MENU_BUTTON),
-            self.driver.find_element(*self.LOGO),
-            self.driver.find_element(*self.SEARCH_BUTTON),
-            self.driver.find_element(*self.NEWS),
-            self.driver.find_element(*self.GALLERY),
-            self.driver.find_element(*self.LIVESTREAM),
-            self.driver.find_element(*self.TRUMP_ACCOUNTS),
-            self.driver.find_element(*self.SAVE_AMERICA),
-            self.driver.find_element(*self.INVESTMENTS),
-            self.driver.find_element(*self.CONTACT),
-        ]
+# from selenium.webdriver.common.by import By
+#
+#
+# class HeaderPage:
+#     MENU_BUTTON = (By.XPATH, "//div[@class='wp-block-whitehouse-header__menu-toggle']//button")
+#
+#     LOGO = (By.XPATH, "//a[@class='wp-block-whitehouse-header__logo']")
+#
+#     SEARCH_BUTTON = (By.XPATH, "//div[@class='wp-block-whitehouse-header__search-toggle']//button")
+#
+#     NEWS = (By.XPATH, "//ul[@id='menu-primary-navigation']//a[contains(text(), 'News')]")
+#     GALLERY = (By.XPATH, "//ul[@id='menu-primary-navigation']//a[contains(text(), 'Gallery')]")
+#     LIVESTREAM = (By.XPATH, "//ul[@id='menu-primary-navigation']//a[contains(text(), 'Livestream')]")
+#     TRUMP_ACCOUNTS = (By.XPATH, "//ul[@id='menu-primary-navigation']//a[contains(text(), 'Trump Accounts')]")
+#     SAVE_AMERICA = (By.XPATH, "//ul[@id='menu-primary-navigation']//a[contains(text(), 'SAVE America')]")
+#     INVESTMENTS = (By.XPATH, "//ul[@id='menu-primary-navigation']//a[contains(text(), 'Investments')]")
+#     CONTACT = (By.XPATH, "//ul[@id='menu-primary-navigation']//a[contains(text(), 'Contact')]")
+#
+#     def __init__(self, driver):
+#         self.driver = driver
+#
+#     def get_header_elements(self):
+#         return [
+#             self.driver.find_element(*self.MENU_BUTTON),
+#             self.driver.find_element(*self.LOGO),
+#             self.driver.find_element(*self.SEARCH_BUTTON),
+#             self.driver.find_element(*self.NEWS),
+#             self.driver.find_element(*self.GALLERY),
+#             self.driver.find_element(*self.LIVESTREAM),
+#             self.driver.find_element(*self.TRUMP_ACCOUNTS),
+#             self.driver.find_element(*self.SAVE_AMERICA),
+#             self.driver.find_element(*self.INVESTMENTS),
+#             self.driver.find_element(*self.CONTACT),
+#         ]
